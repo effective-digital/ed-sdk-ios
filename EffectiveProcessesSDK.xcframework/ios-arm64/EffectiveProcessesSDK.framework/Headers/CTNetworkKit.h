aab7d3ff3497dc693b1a92bb3c6feb627138c1e1
Jasmin Ceco <jasmin.ceco@gmail.com>
2024-06-28 10:06:37 +0200

    init

---
+++ EffectiveProcessesSDK.xcframework/ios-arm64/EffectiveProcessesSDK.framework/Headers/CTNetworkKit.h
@@ -0,0 +1,18 @@
+//
+//  CTNetworkKit.h
+//  CTNetworkKit
+//
+//  Created by Jasmin Ceco on 12/02/2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CTNetworkKit.
+FOUNDATION_EXPORT double CTNetworkKitVersionNumber;
+
+//! Project version string for CTNetworkKit.
+FOUNDATION_EXPORT const unsigned char CTNetworkKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CTNetworkKit/PublicHeader.h>
+
+
